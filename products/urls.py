fab4c90cd0d057c37a0c7e168c5f5d315425a57a
eoinlarkin <eoin.larkin@gmail.com>
2022-08-09 08:20:13 +0100

    refactor: relabelled the main page as home; added text to buttons on product detail page

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='products'),
+    path('', views.index, name='home'),
     path('store', views.store_view, name='store'),
     path('<product_id>', views.product_detail, name='product_detail'),
 ]
