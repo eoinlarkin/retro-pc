94dfb2b601537992b4b8897390d77eb5af41f7a8
eoinlarkin <eoin.larkin@gmail.com>
2022-08-26 08:07:28 +0100

    feat: add base functionality to change address to user profile webpage

--- user_account/views.py
+++ user_account/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
-
+from .forms import UserProfileForm
 
 def user_account(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(instance=profile)
+
+    # geeting the users orders
+    orders = profile.orders.all()
+
     template = 'user_account/account.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
-    return render(request, template, context)
+    return render(request, template, context) 
