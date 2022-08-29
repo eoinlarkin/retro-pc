461f2c2c4156076f7fc8a499e386d007b5906ee5
Eoin Larkin <eoin.larkin@gmail.com>
2022-08-29 14:59:37 +0100

    feat: add profile file and profile view

--- user_account/views.py
+++ user_account/views.py
@@ -1,4 +1,6 @@
+from re import L
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -6,6 +8,15 @@ from .forms import UserProfileForm
 def user_account(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST': 
+
+        # greate a new instance of the User Profile Form using the user data
+        # update the form for the active user
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+             form.save()
+             messages.success(request, 'Profile updated successfully')
 
     form = UserProfileForm(instance=profile)
 
@@ -16,6 +27,9 @@ def user_account(request):
     context = {
         'form': form,
         'orders': orders,
+
+        # flag to check if on profile page; see ci video 061
+        'profile_page_active': True 
     }
 
     return render(request, template, context) 
