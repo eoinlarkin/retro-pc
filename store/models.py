fdbb3f33c911fb31d1f7debbff48d42b09d17fa4
eoinlarkin <eoin.larkin@gmail.com>
2022-08-30 16:50:53 +0100

    feat: update fixtures file with additional product entries

--- store/models.py
+++ store/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     cpu = models.ForeignKey('CPU', null=True, blank=True, on_delete=models.SET_NULL )
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     release_year = models.IntegerField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
