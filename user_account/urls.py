f402741e802a53596c025034223513ee41f4abea
eoinlarkin <eoin.larkin@gmail.com>
2022-08-30 17:35:52 +0100

    feat: add function to checkout/views.py to render the checkout html page

--- user_account/urls.py
+++ user_account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_account, name='profile')
+    path('', views.user_account, name='profile'),
+    path('order_history/<order_number>', views.order_history, name='order_history')
 ]
