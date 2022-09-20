416a2f163f2e8e7d6e577bf697ab2f5c225c5b8f
eoinlarkin <eoin.larkin@gmail.com>
2022-09-20 23:15:41 +0100

    feat: draft stripe webhook integration

--- store/models.py
+++ store/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
 class ReleaseDecade(models.Model):
     release_decade = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.release_decade
+
 
 class Manufacturer(models.Model):
     manufacturer = models.CharField(max_length=254, null=True, blank=True)
@@ -13,18 +14,26 @@ class Manufacturer(models.Model):
     def __str__(self):
         return self.manufacturer
 
+
 class Cpu(models.Model):
     cpu = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.cpu        
+        return self.cpu
+
 
 class Product(models.Model):
-    release_decade = models.ForeignKey('ReleaseDecade', null=True, blank=True, on_delete=models.SET_NULL)
-    manufacturer = models.ForeignKey('Manufacturer', null=True, blank=True, on_delete=models.SET_NULL )
+    release_decade = models.ForeignKey(
+        "ReleaseDecade", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    manufacturer = models.ForeignKey(
+        "Manufacturer", null=True, blank=True, on_delete=models.SET_NULL
+    )
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    cpu = models.ForeignKey('CPU', null=True, blank=True, on_delete=models.SET_NULL )
+    cpu = models.ForeignKey(
+        "CPU", null=True, blank=True, on_delete=models.SET_NULL
+    )
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     release_year = models.IntegerField()
