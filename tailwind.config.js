2ebd142f999d4c3d3a3e8a1cc00922dbaee5ca11
eoinlarkin <eoin.larkin@gmail.com>
2022-09-30 12:51:02 +0100

    style: update formatting / color palette for site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: ["./templates/*.{html,js}",
   "./cart/**/*.{html,js}",
   "./checkout/**/*.{html,js}",
   "./store/**/*.{html,js}",
   "./user_account/**/*.{html,js}",],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'yellow': '#D97706',
+      'blue': '#235789',
+      'black': '#000000',
+      'white': '#F4F4F5',
+      'red': '#9B1D20',
+      'green': '#3F8D48',
+    },
   },
   plugins: [
     require('tw-elements/dist/plugin'),
