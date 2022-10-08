c2797af40744ba6a13d5b684cd1db6568ad38f09
eoinlarkin <eoin.larkin@gmail.com>
2022-10-08 18:24:23 +0100

    style: update formatting of product description page

--- retropc/urls.py
+++ retropc/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
 
 
@@ -37,6 +35,8 @@ urlpatterns = [
         "sitemap.xml",
         TemplateView.as_view(template_name="home/sitemap.xml", content_type="text/xml"),
     ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "retropc.views.handler404"
