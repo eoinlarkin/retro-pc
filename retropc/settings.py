823967eaa3daba6e7102f12262264eda1e8043d3
eoinlarkin <eoin.larkin@gmail.com>
2022-10-08 13:13:12 +0100

    chore: update logic for debug flag

--- retropc/settings.py
+++ retropc/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get("DEBUB_MODE") == "True":
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [
     "retropc.herokuapp.com",
